863541475741ff44fc5e46ee07755e99f1ebe54f
jramos <juanramosgarcia@protonmail.com>
2020-02-18 16:06:59 +0100

    first commit

---
+++ src/main/java/com/mempoolrecorder/MempoolRecorderApplication.java
@@ -0,0 +1,13 @@
+package com.mempoolrecorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MempoolRecorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MempoolRecorderApplication.class, args);
+	}
+
+}
